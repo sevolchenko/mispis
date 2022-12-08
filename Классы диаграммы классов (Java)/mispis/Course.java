2633f34e5e27648a214907a1b12508bda839e08b
ssserchenko <se.volchenko@ya.ru>
2022-12-08 15:31:25 +0300

    initial

---
+++ Классы диаграммы классов (Java)/mispis/Course.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Course {
+
+	public Course(Collection<Lecturer> teaches, String name, int id, float hours) {
+		this.teaches = teaches;
+		this.name = name;
+		this.id = id;
+		this.hours = hours;
+	}
+
+	Collection<Lecturer> teaches;
+	public String name;
+	public int id;
+	public float hours;
+
+	public void numberOfPeopleOnTheCourse(){
+
+	}
+	public void summaryOfGradesForTheCourse(){
+
+	}
+
+}
---
+++ Классы диаграммы классов (Java)/mispis/Faculty.java
@@ -0,0 +1,24 @@
+public class Faculty {
+
+	public Faculty(Employee dean, String name) {
+		this.dean = dean;
+		this.name = name;
+	}
+
+	public Employee dean;
+	public String name;
+
+	public void listOfDestinations(){
+
+	}
+	public void hireTeachers(){
+
+	}
+	public void dismissTeacher(){
+
+	}
+	public void listOfStudents(){
+
+	}
+
+}
